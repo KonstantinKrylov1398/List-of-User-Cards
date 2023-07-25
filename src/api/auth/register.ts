85053abce0c744cde899bfa27b0fa614d4978fde
Константин Крылов <crulov@icloud.com>
2023-07-25 21:03:27 +0300

    рефакторинг кода

--- src/api/auth/register.ts
+++ src/api/auth/register.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { API_URL } from "src/constans";
-import { Auth } from "src/types/auth";
+import { Auth } from "src/types/Auth";
 
 export const register = ({ email, password }: Auth.Entity) =>
   axios.post(`${API_URL}/register`, {
